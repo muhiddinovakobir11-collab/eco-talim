4cc9bfec707f6c63a36f2ee687bbc5a8c1255ef1
muhiddinovakobir11-collab <muhiddinovakobir11@gmail.com>
2026-07-30 18:24:10 +0500

    Implement Eko-Nazorat direct file upload with lightweight admin notification

--- check.js
+++ check.js
@@ -1,6 +1,16 @@
 const https = require('https');
-https.get('https://eco-talim.onrender.com/index.html', (res) => {
-    console.log(res.statusCode);
-}).on('error', (e) => {
-    console.log('ERROR:', e.message);
-});
+
+function check() {
+    https.get('https://eco-talim.onrender.com/index.html', (res) => {
+        if (res.statusCode === 200) {
+            console.log('UP');
+            process.exit(0);
+        } else {
+            setTimeout(check, 5000);
+        }
+    }).on('error', (e) => {
+        setTimeout(check, 5000);
+    });
+}
+
+check();
